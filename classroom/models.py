9be4bbf6a3968bdde85bdfe609bba20ad3a9e36d
Benyamin Noori <benyamin.noori@gmail.com>
2015-08-03 15:25:03 +0430

    models for Classroom app created.

--- classroom/models.py
+++ classroom/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Classroom(models.Model):
+    name = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+
+
+class Schedule(models.Model):
+    classroom = models.ForeignKey(Classroom)
+
+    def __str__(self):
+        return "Schedule for " + self.classroom.name
+
+
+class TimeSlot:
+    schedule = models.ForeignKey(Schedule)
+    begin_time = models.TimeField()
+    end_time = models.TimeField()
+
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=40)
+    classroom = models.ForeignKey(Classroom)
+    time_slot = models.ForeignKey(TimeSlot)
+    activity = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "Class " + self.classroom + " reserved by " + self.name + " for " + self.activity
